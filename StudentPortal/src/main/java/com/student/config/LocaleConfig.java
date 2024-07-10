38f00499554327580440b5c6e9439a0b8a25b8d1
khalidahaldabbagh <147397546+khalidahaldabbagh@users.noreply.github.com>
2024-07-10 23:37:52 +0100

    update

---
+++ StudentPortal/src/main/java/com/student/config/LocaleConfig.java
@@ -0,0 +1,15 @@
+package com.student.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+import java.util.Locale;
+
+@Configuration
+public class LocaleConfig {
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new FixedLocaleResolver(Locale.UK);
+    }
+}
